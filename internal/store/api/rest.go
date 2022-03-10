3fc2d36243f0ad8d7fcd4bb99922f327802b446a
Warren Brown <warrenb95@live.co.uk>
2022-03-10 17:50:20 +0000

    added simple rest server with store

--- internal/store/api/rest.go
+++ internal/store/api/rest.go
@@ -1,7 +1,47 @@
 package api
 
-import "net/http"
+import (
+	"io"
+	"net/http"
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	"github.com/gorilla/mux"
+)
+
+type Store interface {
+	Put(key, value string) error
+	Get(key string) (string, error)
+	Delete(key string) error
+}
+
+type RESTServer struct {
+	Store Store
+}
+
+func (s *RESTServer) IndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello gorilla/mux\n"))
+}
+
+// expects path "/v1/keyvalue/{key}"
+func (s *RESTServer) PUTKeyValueHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	err = s.Store.Put(key, string(value))
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/warrenb95/cloud-native-go/internal/store"
 	"github.com/warrenb95/cloud-native-go/internal/store/api"
 )
 
 func main() {
 	r := mux.NewRouter()
+	store := make(store.Store)
+	server := api.RESTServer{
+		Store: store,
+	}
 
-	r.HandleFunc("/", api.IndexHandler)
+	r.HandleFunc("/", server.IndexHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
